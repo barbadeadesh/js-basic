adf65d2c0b3b3b9c2c2da09b7d092932cae75331
barbadeadesh <barbadeadesh2020@email.com>
2022-05-11 19:33:17 +0530

    ADDED: Functionality for create a function

---
+++ function/Example1.js
@@ -0,0 +1,4 @@
+function display(){
+    console.log("This is my first programm");
+}
+display();
---
+++ function/Example4-user-input.js
@@ -0,0 +1,8 @@
+function add (a,b){
+    return a+b;
+}
+let number1 = Prompt("Enter first number: ");
+let number2= Prompt("Enter second number: ");
+
+let result = add (number1,number2);
+console.log (result);
